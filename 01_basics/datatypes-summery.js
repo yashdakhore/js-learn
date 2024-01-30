e45c26209945375699c4bb449c0887c4f412795e
yashdakhore <yash.p.dakhore@gmail.com>
2024-01-30 20:50:21 +0530

    dev deleted and datatypes added

---
+++ 01_basics/datatypes-summery.js
@@ -0,0 +1,35 @@
+// Primitive DataType
+
+// 7 types : string, Number, Boolean, null, undefined, symbol, BigInt
+
+// const score = 100
+// const scorevalue = 100.3
+
+// const isLoggeIn = false
+// const outsideTemp = null 
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 2342334432432n
+
+
+
+// Refernece (Non Primitive)
+
+// Array, object, Functions
+
+const heros = ["shaktiman", "nagraj", 'doga'];
+let myobj = {
+    name: "yash",
+    age : 26,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
