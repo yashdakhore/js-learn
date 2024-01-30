a00dd406359ae6429561d49bbe93e728c36b9298
yashdakhore <yash.p.dakhore@gmail.com>
2024-01-30 21:20:15 +0530

    datatype summary

--- 01_basics/datatypes-summery.js
+++ 01_basics/datatypes-summery.js
@@ -2,17 +2,17 @@
 
 // 7 types : string, Number, Boolean, null, undefined, symbol, BigInt
 
-// const score = 100
-// const scorevalue = 100.3
+const score = 100
+const scorevalue = 100.3
 
-// const isLoggeIn = false
-// const outsideTemp = null 
-// let userEmail;
+const isLoggeIn = false
+const outsideTemp = null 
+let userEmail;
 
-// const id = Symbol('123')
-// const anotherId = Symbol('123')
+const id = Symbol('123')
+const anotherId = Symbol('123')
 
-// console.log(id === anotherId);
+console.log(id === anotherId);
 
 const bigNumber = 2342334432432n
 
@@ -32,4 +32,4 @@ const myFunction = function(){
     console.log("Hello World");
 }
 
-console.log(typeof bigNumber);
+console.log(typeof anotherId);
