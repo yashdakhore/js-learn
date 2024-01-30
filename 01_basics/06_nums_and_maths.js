578dfd71257253621ad739937596cb05fda45a51
yashdakhore <yash.p.dakhore@gmail.com>
2024-01-30 23:14:13 +0530

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,36 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number (100)
+console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.2332
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// ***********************MATHS*************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,9, 6, 8, 0));
+// console.log(Math.max(4,9, 6, 8, 0));
+
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10) +1);
+
+
+const min = 10 
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
